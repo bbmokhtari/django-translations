98090e6f915768f62e9571e19f3ff722d858e87e
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-07-08 12:38:10 +0430

    First test created for the Translations app

--- example/places/tests.py
+++ example/places/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
+from django.db import utils
+from translations.models import Translation
 
-# Create your tests here.
+from .models import Continent
+
+
+class ContinentTest(TestCase):
+
+    def test_translation_content_type_empty(self):
+        continent = Continent.objects.create(
+            name="Europe",
+            code="EU",
+        )
+        with self.assertRaises(utils.IntegrityError) as integrityError:
+            Translation.objects.create(
+                content_type=None,
+                object_id=continent.id,
+                field="name",
+                language="fr",
+                text="L'Europe"
+            )
+        self.assertEqual(
+            integrityError.exception.args[0],
+            "NOT NULL constraint failed: translations_translation.content_type_id",
+        )
