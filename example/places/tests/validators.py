5020fc5a5b75e926cf4ac1ae1001d1bb3af3dd6c
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-07-14 12:25:10 +0430

    validators test has been written

---
+++ example/places/tests/validators.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from translations.validators import validate_language
+
+
+class ValidatorsTest(TestCase):
+
+    def test_validate_language(self):
+        self.assertIs(validate_language('en'), None)
+
+    def test_validate_language_invalid(self):
+        with self.assertRaises(ValidationError) as error:
+            validate_language('xx')
+        self.assertEqual(
+            error.exception.args[0],
+            "The language code `xx` is not supported."
+        )
