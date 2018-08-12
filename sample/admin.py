2affb956fa00549b73daf0892b5882b1e3d15258
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-08-12 18:34:37 +0430

    sample app admin based on readme

--- sample/admin.py
+++ sample/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from translations.admin import TranslatableAdmin, TranslationInline
 
-# Register your models here.
+from .models import Continent, Country, City
+
+
+class ContinentAdmin(TranslatableAdmin):
+    inlines = [TranslationInline]
+
+
+class CountryAdmin(TranslatableAdmin):
+    inlines = [TranslationInline]
+
+
+class CityAdmin(TranslatableAdmin):
+    inlines = [TranslationInline]
+
+admin.site.register(Continent, ContinentAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(City, CityAdmin)
