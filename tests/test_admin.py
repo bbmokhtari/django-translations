33601062e1bdcdcaa009f2dfac72d407050b19d4
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-08-31 21:54:58 +0430

    tests for admin completed

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -5,6 +5,19 @@ from sample.models import Continent, City, Geo
 from sample.admin import ContinentAdmin, CityAdmin, GeoAdmin
 
 
+class MockRequest:
+    pass
+
+
+class MockSuperUser:
+    def has_perm(self, perm):
+        return True
+
+
+request = MockRequest()
+request.user = MockSuperUser()
+
+
 class TranslatableAdminMixin(TestCase):
 
     def test_get_translation_choices_none(self):
@@ -25,5 +38,29 @@ class TranslatableAdminMixin(TestCase):
         admin = ContinentAdmin(Continent, site)
         self.assertListEqual(
             admin._get_translation_choices(),
+            [(None, '---------'), ('name', 'name'), ('denonym', 'denonym')]
+        )
+
+    def test_handle_translation_inlines_none(self):
+        admin = CityAdmin(City, site)
+        inlines = admin.get_inline_instances(request, obj=None)
+        self.assertListEqual(
+            inlines[0].form.base_fields['field'].choices,
+            [(None, '---------'), ('name', 'name'), ('denonym', 'denonym')]
+        )
+
+    def test_handle_translation_inlines_empty(self):
+        admin = GeoAdmin(Geo, site)
+        inlines = admin.get_inline_instances(request, obj=None)
+        self.assertListEqual(
+            inlines,
+            []
+        )
+
+    def test_handle_translation_inlines_explicit(self):
+        admin = ContinentAdmin(Continent, site)
+        inlines = admin.get_inline_instances(request, obj=None)
+        self.assertEqual(
+            inlines[0].form.base_fields['field'].choices,
             [(None, '---------'), ('name', 'name'), ('denonym', 'denonym')]
         )
