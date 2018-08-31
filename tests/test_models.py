abe258e789aab859c65ec74d057aaf6182ecab4a
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-08-31 17:48:17 +0430

    `Translation` unit tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,147 +1,150 @@
-# from django.test import TestCase
-# from django.contrib.contenttypes.models import ContentType
-# from django.db import utils
+from django.test import TestCase
+from django.contrib.contenttypes.models import ContentType
+from django.db import utils
 
-# from translations.models import Translation
+from translations.models import Translation
 
-# from sample.models import Continent, Country, City
+from sample.models import Continent, Country, City
 
 
-# class TranslationTest(TestCase):
+class TranslationTest(TestCase):
 
-#     def test_content_type_none(self):
-#         """Make sure `content_type` can not be null."""
-#         europe = Continent.objects.create(name="Europe", code="EU")
-#         with self.assertRaises(utils.IntegrityError) as error:
-#             Translation.objects.create(
-#                 content_type=None,
-#                 object_id=europe.id,
-#                 field="name",
-#                 language="de",
-#                 text="Europa",
-#             )
-#         self.assertEqual(
-#             error.exception.args[0],
-#             "NOT NULL constraint failed: translations_translation.content_type_id",
-#         )
+    def test_content_type_none(self):
+        """Make sure `content_type` can not be null."""
+        europe = Continent.objects.create(name="Europe", code="EU")
+        with self.assertRaises(utils.IntegrityError) as error:
+            Translation.objects.create(
+                content_type=None,
+                object_id=europe.id,
+                field="name",
+                language="de",
+                text="Europa",
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "NOT NULL constraint failed: translations_translation.content_type_id",
+        )
 
-#     def test_object_id_none(self):
-#         """Make sure `object_id` can not be null."""
-#         continent_ct = ContentType.objects.get_for_model(Continent)
-#         with self.assertRaises(utils.IntegrityError) as error:
-#             Translation.objects.create(
-#                 content_type=continent_ct,
-#                 object_id=None,
-#                 field="name",
-#                 language="de",
-#                 text="Europa",
-#             )
-#         self.assertEqual(
-#             error.exception.args[0],
-#             "NOT NULL constraint failed: translations_translation.object_id",
-#         )
+    def test_object_id_none(self):
+        """Make sure `object_id` can not be null."""
+        continent_ct = ContentType.objects.get_for_model(Continent)
+        with self.assertRaises(utils.IntegrityError) as error:
+            Translation.objects.create(
+                content_type=continent_ct,
+                object_id=None,
+                field="name",
+                language="de",
+                text="Europa",
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "NOT NULL constraint failed: translations_translation.object_id",
+        )
 
-#     def test_content_object_none(self):
-#         """Make sure `content_object` can not be null."""
-#         with self.assertRaises(utils.IntegrityError) as error:
-#             Translation.objects.create(
-#                 content_object=None,
-#                 field="name",
-#                 language="de",
-#                 text="Europa",
-#             )
-#         self.assertEqual(
-#             error.exception.args[0],
-#             "NOT NULL constraint failed: translations_translation.object_id",
-#         )
+    def test_content_object_none(self):
+        """Make sure `content_object` can not be null."""
+        with self.assertRaises(utils.IntegrityError) as error:
+            Translation.objects.create(
+                content_object=None,
+                field="name",
+                language="de",
+                text="Europa",
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "NOT NULL constraint failed: translations_translation.object_id",
+        )
 
-#     def test_field_none(self):
-#         """Make sure `field` can not be null."""
-#         europe = Continent.objects.create(name="Europe", code="EU")
-#         continent_ct = ContentType.objects.get_for_model(Continent)
-#         with self.assertRaises(utils.IntegrityError) as error:
-#             Translation.objects.create(
-#                 content_type=continent_ct,
-#                 object_id=europe.id,
-#                 field=None,
-#                 language="de",
-#                 text="Europa",
-#             )
-#         self.assertEqual(
-#             error.exception.args[0],
-#             "NOT NULL constraint failed: translations_translation.field",
-#         )
+    def test_field_none(self):
+        """Make sure `field` can not be null."""
+        europe = Continent.objects.create(name="Europe", code="EU")
+        continent_ct = ContentType.objects.get_for_model(Continent)
+        with self.assertRaises(utils.IntegrityError) as error:
+            Translation.objects.create(
+                content_type=continent_ct,
+                object_id=europe.id,
+                field=None,
+                language="de",
+                text="Europa",
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "NOT NULL constraint failed: translations_translation.field",
+        )
 
-#     def test_language_none(self):
-#         """Make sure `language` can not be null."""
-#         europe = Continent.objects.create(name="Europe", code="EU")
-#         continent_ct = ContentType.objects.get_for_model(Continent)
-#         with self.assertRaises(utils.IntegrityError) as error:
-#             Translation.objects.create(
-#                 content_type=continent_ct,
-#                 object_id=europe.id,
-#                 field="name",
-#                 language=None,
-#                 text="Europa",
-#             )
-#         self.assertEqual(
-#             error.exception.args[0],
-#             "NOT NULL constraint failed: translations_translation.language",
-#         )
+    def test_language_none(self):
+        """Make sure `language` can not be null."""
+        europe = Continent.objects.create(name="Europe", code="EU")
+        continent_ct = ContentType.objects.get_for_model(Continent)
+        with self.assertRaises(utils.IntegrityError) as error:
+            Translation.objects.create(
+                content_type=continent_ct,
+                object_id=europe.id,
+                field="name",
+                language=None,
+                text="Europa",
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "NOT NULL constraint failed: translations_translation.language",
+        )
 
-#     def test_text_none(self):
-#         """Make sure text can not be null."""
-#         europe = Continent.objects.create(name="Europe", code="EU")
-#         continent_ct = ContentType.objects.get_for_model(Continent)
-#         with self.assertRaises(utils.IntegrityError) as error:
-#             Translation.objects.create(
-#                 content_type=continent_ct,
-#                 object_id=europe.id,
-#                 field="name",
-#                 language="de",
-#                 text=None,
-#             )
-#         self.assertEqual(
-#             error.exception.args[0],
-#             "NOT NULL constraint failed: translations_translation.text",
-#         )
+    def test_text_none(self):
+        """Make sure text can not be null."""
+        europe = Continent.objects.create(name="Europe", code="EU")
+        continent_ct = ContentType.objects.get_for_model(Continent)
+        with self.assertRaises(utils.IntegrityError) as error:
+            Translation.objects.create(
+                content_type=continent_ct,
+                object_id=europe.id,
+                field="name",
+                language="de",
+                text=None,
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "NOT NULL constraint failed: translations_translation.text",
+        )
 
-#     def test_str(self):
-#         """Make sure `__str__` returns source and translation."""
-#         europe = Continent.objects.create(name="Europe", code="EU")
-#         continent_ct = ContentType.objects.get_for_model(Continent)
-#         translation = Translation.objects.create(
-#             content_type=continent_ct,
-#             object_id=europe.id,
-#             field="name",
-#             language="de",
-#             text="Europa"
-#         )
-#         self.assertEqual(str(translation), "Europe: Europa")
+    def test_str(self):
+        """Make sure `__str__` returns source and translation."""
+        europe = Continent.objects.create(name="Europe", code="EU")
+        continent_ct = ContentType.objects.get_for_model(Continent)
+        translation = Translation.objects.create(
+            content_type=continent_ct,
+            object_id=europe.id,
+            field="name",
+            language="de",
+            text="Europa"
+        )
+        self.assertEqual(str(translation), "Europe: Europa")
 
-#     def test_uniqueness(self):
-#         """Make sure `content_type`, `object_id`, `field` and `language` combination is unique."""
-#         europe = Continent.objects.create(name="Europe", code="EU")
-#         continent_ct = ContentType.objects.get_for_model(Continent)
-#         Translation.objects.create(
-#             content_type=continent_ct,
-#             object_id=europe.id,
-#             field="name",
-#             language="de",
-#             text="Europa"
-#         )
-#         with self.assertRaises(utils.IntegrityError) as error:
-#             Translation.objects.create(
-#                 content_type=continent_ct,
-#                 object_id=europe.id,
-#                 field="name",
-#                 language="de",
-#                 text="Europa"
-#             )
-#         self.assertEqual(
-#             error.exception.args[0],
-#             "UNIQUE constraint failed: translations_translation.content_type_id, translations_translation.object_id, translations_translation.field, translations_translation.language",
-#         )
+    def test_uniqueness(self):
+        """
+        Make sure `content_type`, `object_id`, `field` and `language`
+        combination is unique.
+        """
+        europe = Continent.objects.create(name="Europe", code="EU")
+        continent_ct = ContentType.objects.get_for_model(Continent)
+        Translation.objects.create(
+            content_type=continent_ct,
+            object_id=europe.id,
+            field="name",
+            language="de",
+            text="Europa"
+        )
+        with self.assertRaises(utils.IntegrityError) as error:
+            Translation.objects.create(
+                content_type=continent_ct,
+                object_id=europe.id,
+                field="name",
+                language="de",
+                text="Europa"
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "UNIQUE constraint failed: translations_translation.content_type_id, translations_translation.object_id, translations_translation.field, translations_translation.language",
+        )
 
 
 # class TranslatableTest(TestCase):
