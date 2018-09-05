9e2fbd15613ba87b726c1a6583bcf764bd9a5a3b
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-09-05 21:57:33 +0430

    `TranslatableQuerySet.apply_translations` documented

--- translations/querysets.py
+++ translations/querysets.py
@@ -3,11 +3,10 @@ This module contains the querysets for the Translations app. It contains the
 following members:
 
 :class:`TranslatableQuerySet`
-    A :class:`~django.db.models.QuerySet` which provides custom
-    translation functionalities.
+    A queryset which provides custom translation functionalities.
 """
 
-from django.db import models, transaction
+from django.db import models
 
 from translations.utils import apply_translations, update_translations
 
@@ -19,48 +18,37 @@ class TranslatableQuerySet(models.QuerySet):
     """
     A queryset which provides custom translation functionalities.
 
-    Provides functionalities like :meth:`apply_translations` to read and apply
-    translations from the database onto the queryset, and
-    :meth:`update_translations` to write and update translations from the
-    queryset onto the database.
+    Provides functionalities like :meth:`apply_translations` to read the
+    translations from the database and apply them on the queryset, and
+    :meth:`update_translations` to update the translations from the queryset
+    and write them on the database.
     """
 
     def apply_translations(self, *relations, lang=None):
         """
-        Apply the translations on the queryset and the relations of it in a
+        Apply the translations of the queryset and some relations of it in a
         language.
 
         Fetches the translations of the queryset and the specified relations
-        of it in a language and applies them field by field.
+        of it in a language and applies them on the translatable
+        :attr:`~translations.models.Translatable.TranslatableMeta.fields` of
+        the queryset and the relations of it.
 
         :param relations: The relations of the queryset to apply the
-            translations on.
+            translations of.
         :type relations: list(str)
         :param lang: The language to fetch the translations in.
             ``None`` means use the :term:`active language` code.
         :type lang: str or None
+        :return: The list of instance which the translations of have been
+            applied on.
+        :rtype: list(~translations.models.Translatable)
         :raise ValueError: If the language code is not included in
             the :data:`~django.conf.settings.LANGUAGES` setting.
+        :raise TypeError: If the models of the included relations
+            are not :class:`~translations.models.Translatable`.
         :raise ~django.core.exceptions.FieldDoesNotExist: If a relation is
             pointing to the fields that don't exist.
-
-        .. warning::
-           The relations of the queryset **must** be fetched before performing
-           the translation process.
-
-           To do this use
-           :meth:`~django.db.models.query.QuerySet.select_related`,
-           :meth:`~django.db.models.query.QuerySet.prefetch_related` or
-           :func:`~django.db.models.prefetch_related_objects`.
-
-        .. warning::
-           Only when all the filterings are executed on the relations of the
-           queryset it should go through the translation process, otherwise if
-           a relation is filtered after the translation process the
-           translations of that relation are reset.
-
-           To filter a relation when fetching it use
-           :class:`~django.db.models.Prefetch`.
 
         .. testsetup:: apply_translations
 
@@ -76,11 +64,30 @@ class TranslatableQuerySet(models.QuerySet):
                langs=['de']
            )
 
-        To apply the translations on a queryset and the relations of it:
+        .. note::
+
+           If there is no translation for a field in translatable
+           :attr:`~translations.models.Translatable.TranslatableMeta.fields`,
+           the translation of the field falls back to the value of the field
+           in the instance.
+
+        .. note::
+
+           It is **recommended** for the relations of the queryset to be
+           prefetched before applying the translations in order to reach
+           optimal performance.
+
+           To do this use
+           :meth:`~django.db.models.query.QuerySet.select_related`,
+           :meth:`~django.db.models.query.QuerySet.prefetch_related` or
+           :func:`~django.db.models.prefetch_related_objects`.
+
+        To apply the translations of a queryset and the relations of it:
 
         .. testcode:: apply_translations
 
            from sample.models import Continent
+           from translations.utils import apply_translations
 
            relations = ('countries', 'countries__cities',)
 
@@ -108,9 +115,92 @@ class TranslatableQuerySet(models.QuerySet):
            Country: Südkorea
            City: Seül
            City: Ulsän
+
+        .. warning::
+
+           Filtering any queryset after applying the translations will cause
+           the translations of that queryset to be reset. The solution is to
+           do the filtering before applying the translations.
+
+           To do this on the relations use :class:`~django.db.models.Prefetch`.
+
+           Consider this case:
+
+           .. testcode:: apply_translations
+
+              from sample.models import Continent
+              from translations.utils import apply_translations
+
+              relations = ('countries', 'countries__cities',)
+
+              continents = Continent.objects.prefetch_related(
+                  *relations
+              ).apply_translations(
+                  *relations,
+                  lang='de'
+              )
+
+              for continent in continents:
+                  print('Continent: {}'.format(continent))
+                  for country in continent.countries.exclude(name=''):  # Wrong
+                      print('Country: {}  -- Wrong'.format(country))
+                      for city in country.cities.all():
+                          print('City: {}  -- Wrong'.format(city))
+
+           .. testoutput:: apply_translations
+
+              Continent: Europa
+              Country: Germany  -- Wrong
+              City: Cologne  -- Wrong
+              City: Munich  -- Wrong
+              Continent: Asien
+              Country: South Korea  -- Wrong
+              City: Seoul  -- Wrong
+              City: Ulsan  -- Wrong
+
+           As we can see the translations of the filtered queryset are reset.
+           To fix it:
+
+           .. testcode:: apply_translations
+
+              from django.db.models import Prefetch
+              from sample.models import Continent, Country
+              from translations.utils import apply_translations
+
+              relations = ('countries', 'countries__cities',)
+
+              continents = Continent.objects.prefetch_related(
+                  Prefetch(
+                      'countries',
+                      queryset=Country.objects.exclude(name='')  # Correct
+                  ),
+                  'countries__cities',
+              ).apply_translations(
+                  *relations,
+                  lang='de'
+              )
+
+              for continent in continents:
+                  print('Continent: {}'.format(continent))
+                  for country in continent.countries.all():
+                      print('Country: {}'.format(country))
+                      for city in country.cities.all():
+                          print('City: {}'.format(city))
+
+           .. testoutput:: apply_translations
+
+              Continent: Europa
+              Country: Deutschland
+              City: Köln
+              City: München
+              Continent: Asien
+              Country: Südkorea
+              City: Seül
+              City: Ulsän
         """
-        apply_translations(self, *relations, lang=lang)
-        return self
+        clone = self._chain()
+        apply_translations(clone, *relations, lang=lang)
+        return list(clone)
 
     def update_translations(self, *relations, lang=None):
         """
