8290a3e2c630e7d7fddc803f9a6810b9827c8bbb
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-09-20 17:16:40 +0430

    removed geo info and added the timezone models to sample app

--- sample/admin.py
+++ sample/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from translations.admin import TranslatableAdmin, TranslationInline
 
-from .models import Continent, Country, City
+from .models import Timezone, Continent, Country, City
 
 
-class GeoAdmin(TranslatableAdmin):
+class TimezoneAdmin(TranslatableAdmin):
     inlines = [TranslationInline]
 
 
@@ -20,6 +20,7 @@ class CityAdmin(TranslatableAdmin):
     inlines = [TranslationInline]
 
 
+admin.site.register(Timezone, TimezoneAdmin)
 admin.site.register(Continent, ContinentAdmin)
 admin.site.register(Country, CountryAdmin)
 admin.site.register(City, CityAdmin)
