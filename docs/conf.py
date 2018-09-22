ae847c7baa517e78fe530e06c40c5dbc2a956142
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-09-22 22:10:53 +0330

    new theme configs for codes inside the notes

--- docs/conf.py
+++ docs/conf.py
@@ -128,7 +128,10 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'note_bg': '#fec',
+    'note_border': '#ffe2a8',
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
