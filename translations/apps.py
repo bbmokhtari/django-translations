fa84b1e2e3b6b7edc6b28aa922d8f4bf0bba4a38
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-10-03 23:42:14 +0330

    caching of all the content types in the start of the django proj

--- translations/apps.py
+++ translations/apps.py
@@ -5,3 +5,9 @@ from django.utils.translation import ugettext_lazy as _
 class TranslationsConfig(AppConfig):
     name = 'translations'
     verbose_name = _('translations')
+
+    def ready(self):
+        # cache all content types at the start
+        from django.contrib.contenttypes.models import ContentType
+        models = [ct.model_class() for ct in ContentType.objects.all()]
+        ContentType.objects.get_for_models(*models)
