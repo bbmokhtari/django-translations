59d546b3fcf3c1ed4ef2124ac55950709f405166
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-10-04 16:25:02 +0330

    model documentation moved to docs

--- translations/models.py
+++ translations/models.py
@@ -1,12 +1,4 @@
-"""
-This module contains the models for the Translations app. It contains the
-following members:
-
-:class:`Translation`
-    The model which represents the translations.
-:class:`Translatable`
-    An abstract model which provides custom translation functionalities.
-"""
+"""This module contains the models for the Translations app."""
 
 from django.db import models
 from django.conf import settings
@@ -20,65 +12,7 @@ __docformat__ = 'restructuredtext'
 
 
 class Translation(models.Model):
-    """
-    The model which represents the translations.
-
-    Each translation belongs to a *unique* database address. Each address is
-    composed of a :attr:`content_type` (table), an :attr:`object_id` (row) and
-    a :attr:`field` (column). Each unique address must have only one
-    translation :attr:`text` in a specific :attr:`language`.
-
-    .. note::
-
-       :attr:`content_type` and :attr:`object_id` together form something
-       called a :class:`~django.contrib.contenttypes.fields.GenericForeignKey`.
-       This kind of foreign key contrary to the normal foreign key (which can
-       point to a row in only one table) can point to a row in any table.
-
-    .. note::
-
-       :attr:`object_id` is defined as a :class:`~django.db.models.CharField`
-       so that it can also point to the rows in the tables which use character
-       fields (like :class:`~django.db.models.UUIDField`, etc.) as primary key.
-
-    .. warning::
-
-       Try **not** to work with the :class:`~translations.models.Translation`
-       model directly unless you *really* have to and you know what you're
-       doing.
-
-    To create the translation of a field manually:
-
-    .. testsetup:: Translation
-
-       from tests.sample import create_samples
-
-       create_samples(
-           continent_names=['europe'],
-       )
-
-    .. testcode:: Translation
-
-       from django.contrib.contenttypes.models import ContentType
-       from sample.models import Continent
-       from translations.models import Translation
-
-       europe = Continent.objects.get(code='EU')
-
-       translation = Translation.objects.create(
-           content_type=ContentType.objects.get_for_model(Continent),
-           object_id=europe.id,
-           field='name',
-           language='de',
-           text='Europa',
-       )
-
-       print(translation)
-
-    .. testoutput:: Translation
-
-       Europe: Europa
-    """
+    """The model which represents the translations."""
 
     content_type = models.ForeignKey(
         verbose_name=_('content type'),
@@ -125,22 +59,7 @@ class Translation(models.Model):
 
 
 class Translatable(models.Model):
-    """
-    An abstract model which provides custom translation functionalities.
-
-    Marks the subclasses as translatable and creates some default
-    configurations for them based on their structure.
-
-    It also adds the :attr:`translations` relation to the model, just in case
-    any one wants to work with the translations of an instance manually.
-
-    .. note::
-
-       The :attr:`translations` relation is the reverse relation of the
-       :class:`~django.contrib.contenttypes.fields.GenericForeignKey`
-       described in :class:`~translations.models.Translation`. It's a
-       :class:`~django.contrib.contenttypes.fields.GenericRelation`.
-    """
+    """An abstract model which provides custom translation functionalities."""
 
     translations = GenericRelation(
         Translation,
@@ -152,37 +71,17 @@ class Translatable(models.Model):
     class Meta:
         abstract = True
 
+    class TranslatableMeta:
+        """
+        This class contains meta information about the translation
+        of the model instances.
+        """
+
+        fields = None
+
     @classmethod
     def get_translatable_fields(cls):
-        """
-        Return the translatable fields of the model.
-
-        Returns the translatable fields of the model based on the
-        field names listed in :attr:`TranslatableMeta.fields`.
-
-        :return: The translatable fields of the model.
-        :rtype: list(~django.db.models.Field)
-
-        Considering this model:
-
-        .. literalinclude:: ../../sample/models.py
-           :pyobject: Continent
-           :emphasize-lines: 27-28
-
-        To get the translatable fields of the mentioned model:
-
-        .. testcode:: get_translatable_fields
-
-           from sample.models import Continent
-
-           for field in Continent.get_translatable_fields():
-               print(field)
-
-        .. testoutput:: get_translatable_fields
-
-           sample.Continent.name
-           sample.Continent.denonym
-        """
+        """Return the translatable fields of the model."""
         if not hasattr(cls, '_cached_translatable_fields'):
             if cls.TranslatableMeta.fields is None:
                 fields = []
@@ -207,35 +106,7 @@ class Translatable(models.Model):
 
     @classmethod
     def _get_translatable_fields_names(cls):
-        """
-        Return the names of the model's translatable fields.
-
-        Returns the names of the model's translatable fields based on the
-        field names listed in :attr:`TranslatableMeta.fields`.
-
-        :return: The names of the model's translatable fields.
-        :rtype: list(str)
-
-        Considering this model:
-
-        .. literalinclude:: ../../sample/models.py
-           :pyobject: Continent
-           :emphasize-lines: 27-28
-
-        To get the names of the mentioned model's translatable fields:
-
-        .. testcode:: _get_translatable_fields_names
-
-           from sample.models import Continent
-
-           for name in Continent._get_translatable_fields_names():
-               print(name)
-
-        .. testoutput:: _get_translatable_fields_names
-
-           name
-           denonym
-        """
+        """Return the names of the model's translatable fields."""
         if not hasattr(cls, '_cached_translatable_fields_names'):
             cls._cached_translatable_fields_names = [
                 field.name for field in cls.get_translatable_fields()
@@ -244,36 +115,7 @@ class Translatable(models.Model):
 
     @classmethod
     def _get_translatable_fields_choices(cls):
-        """
-        Return the choices of the model's translatable fields.
-
-        Returns the choices of the model's translatable fields based on the
-        field names listed in :attr:`TranslatableMeta.fields`.
-
-        :return: The choices of the model's translatable fields.
-        :rtype: list(tuple(str, str))
-
-        Considering this model:
-
-        .. literalinclude:: ../../sample/models.py
-           :pyobject: Continent
-           :emphasize-lines: 27-28
-
-        To get the choices of the mentioned model's translatable fields:
-
-        .. testcode:: _get_translatable_fields_choices
-
-           from sample.models import Continent
-
-           for choice in Continent._get_translatable_fields_choices():
-               print(choice)
-
-        .. testoutput:: _get_translatable_fields_choices
-
-           (None, '---------')
-           ('name', 'name')
-           ('denonym', 'denonym')
-        """
+        """Return the choices of the model's translatable fields."""
         choices = [
             (None, '---------'),
         ]
@@ -283,17 +125,3 @@ class Translatable(models.Model):
             choices.append(choice)
 
         return choices
-
-    class TranslatableMeta:
-        """
-        The class which contains meta information about the translation
-        of the model instances.
-        """
-
-        fields = None
-        """
-        :var fields: The names of the fields to use in the translation.
-            ``None`` means use the text based fields automatically.
-            ``[]`` means use no fields.
-        :vartype fields: list(str) or None
-        """
