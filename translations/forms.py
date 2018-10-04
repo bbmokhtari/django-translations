a5be3a2087d14e67bf1342a4bdc6de3cd8efb22f
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-10-04 16:35:10 +0330

    translations forms moved to the docs

--- translations/forms.py
+++ translations/forms.py
@@ -1,10 +1,4 @@
-"""
-This module contains the forms for the Translations app. It contains the
-following members:
-
-:func:`generate_translation_form`
-    Return a translation form based on a translatable model.
-"""
+"""This module contains the forms for the Translations app."""
 
 from django import forms
 
@@ -15,18 +9,7 @@ __docformat__ = 'restructuredtext'
 
 
 def generate_translation_form(translatable):
-    """
-    Return a translation form based on a translatable model.
-
-    Generates the translation form based on the translatable fields of the
-    translatable model and returns it.
-
-    :param translatable: The translatable model to generate the translation
-        form based on.
-    :type translatable: type(~translations.models.Translatable)
-    :return: The translation form generated based on the translatable model.
-    :rtype: type(~django.forms.ModelForm(~translations.models.Translation))
-    """
+    """Return a translation form based on a translatable model."""
     choices = translatable._get_translatable_fields_choices()
 
     class TranslationForm(forms.ModelForm):
