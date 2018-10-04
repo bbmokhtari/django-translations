ad4896c56ca0ad3838fd50b9543957ef734a8ef4
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-10-04 16:47:20 +0330

    admin docs moved to the docs/reference

--- docs/reference/admin.rst
+++ docs/reference/admin.rst
@@ -2,12 +2,71 @@
 Admin
 *****
 
-.. automodule:: translations.admin
+.. module:: translations.admin
 
-   .. autoclass:: TranslatableAdminMixin
+This module contains the admins for the Translations app.
 
-      .. automethod:: prepare_translation_inlines
+.. class:: TranslatableAdminMixin
 
-   .. autoclass:: TranslatableAdmin
+   An admin mixin which provides custom translation functionalities.
 
-   .. autoclass:: TranslationInline
+   Provides functionalities like :meth:`prepare_translation_inlines` to
+   prepare the translation inlines of a type in some inlines based on the
+   admin model.
+
+   .. method:: prepare_translation_inlines(self, inlines, inline_type)
+
+      Prepare the translation inlines of a type in some inlines based on the
+      admin model.
+
+      Searches the inlines for the translation inlines of the specified
+      inline type and prepares the translation inlines based on the admin
+      model.
+
+      :param inlines: The inlines which contain the translation inlines to
+          prepare.
+      :type inlines: list(~django.contrib.admin.InlineModelAdmin)
+      :param inline_type: The type of the translation inlines.
+      :type inline_type: type(~django.contrib.contenttypes.admin.\\
+          GenericStackedInline)
+
+      To prepare the translation inlines of a type in some inlines based on
+      the admin model:
+
+      .. literalinclude:: ../../translations/admin.py
+         :pyobject: TranslationInline
+
+      .. literalinclude:: ../../translations/admin.py
+         :pyobject: TranslatableAdmin
+         :emphasize-lines: 11
+
+      .. note::
+
+         The code above is exactly how the Translations app makes Django
+         admin translatable. It can be used to make any admin translatable.
+
+         Check out :doc:`../howto/customadmin`.
+
+.. class:: TranslatableAdmin
+
+   The admin which represents the translatables.
+
+   Manages creating, reading, updating and deleting the translatable objects.
+
+   To make an admin translatable:
+
+   .. literalinclude:: ../../sample/admin.py
+      :pyobject: ContinentAdmin
+      :emphasize-lines: 1
+
+.. class:: TranslationInline
+
+   The inline which represents the translations.
+
+   Manages creating, reading, updating and deleting the translation objects.
+
+   To add translation inlines to a translatable admin:
+
+   .. literalinclude:: ../../sample/admin.py
+      :pyobject: ContinentAdmin
+      :emphasize-lines: 2
--- translations/admin.py
+++ translations/admin.py
@@ -1,14 +1,4 @@
-"""
-This module contains the admins for the Translations app. It contains the
-following members:
-
-:class:`TranslatableAdminMixin`
-    An admin mixin which provides custom translation functionalities.
-:class:`TranslatableAdmin`
-    The admin which represents the translatables.
-:class:`TranslationInline`
-    The inline which represents the translations.
-"""
+"""This module contains the admins for the Translations app."""
 
 from django.contrib.contenttypes.admin import GenericStackedInline
 from django.contrib import admin
@@ -21,48 +11,12 @@ __docformat__ = 'restructuredtext'
 
 
 class TranslatableAdminMixin(object):
-    """
-    An admin mixin which provides custom translation functionalities.
-
-    Provides functionalities like :meth:`prepare_translation_inlines` to
-    prepare the translation inlines of a type in some inlines based on the
-    admin model.
-    """
+    """An admin mixin which provides custom translation functionalities."""
 
     def prepare_translation_inlines(self, inlines, inline_type):
         """
         Prepare the translation inlines of a type in some inlines based on the
         admin model.
-
-        Searches the inlines for the translation inlines of the specified
-        inline type and prepares the translation inlines based on the admin
-        model.
-
-        :param inlines: The inlines which contain the translation inlines to
-            prepare.
-        :type inlines: list(~django.contrib.admin.InlineModelAdmin)
-        :param inline_type: The type of the translation inlines.
-        :type inline_type: type(~django.contrib.contenttypes.admin.\\
-            GenericStackedInline)
-
-        To prepare the translation inlines of a type in some inlines based on
-        the admin model:
-
-        .. literalinclude:: ../../translations/admin.py
-           :pyobject: TranslationInline
-           :lines: 1, 14-
-
-        .. literalinclude:: ../../translations/admin.py
-           :pyobject: TranslatableAdmin
-           :lines: 1, 14-
-           :emphasize-lines: 9
-
-        .. note::
-
-           The code above is exactly how the Translations app makes Django
-           admin translatable. It can be used to make any admin translatable.
-
-           Check out :doc:`../howto/customadmin`.
         """
         form = generate_translation_form(self.model)
         remove_inlines = []
@@ -78,17 +32,7 @@ class TranslatableAdminMixin(object):
 
 
 class TranslatableAdmin(TranslatableAdminMixin, admin.ModelAdmin):
-    """
-    The admin which represents the translatables.
-
-    Manages creating, reading, updating and deleting the translatable objects.
-
-    To make an admin translatable:
-
-    .. literalinclude:: ../../sample/admin.py
-       :pyobject: ContinentAdmin
-       :emphasize-lines: 1
-    """
+    """The admin which represents the translatables."""
 
     def get_inline_instances(self, request, obj=None):
         inlines = list(
@@ -102,17 +46,7 @@ class TranslatableAdmin(TranslatableAdminMixin, admin.ModelAdmin):
 
 
 class TranslationInline(GenericStackedInline):
-    """
-    The inline which represents the translations.
-
-    Manages creating, reading, updating and deleting the translation objects.
-
-    To add translation inlines to a translatable admin:
-
-    .. literalinclude:: ../../sample/admin.py
-       :pyobject: ContinentAdmin
-       :emphasize-lines: 2
-    """
+    """The inline which represents the translations."""
 
     model = Translation
     extra = 1
