2e9216d2318409aea142becf4cc8049bc131d031
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-10-05 17:59:45 +0330

    do not include the default language from the admin

--- translations/forms.py
+++ translations/forms.py
@@ -1,8 +1,10 @@
 """This module contains the forms for the Translations app."""
 
 from django import forms
+from django.conf import settings
 
 from translations.models import Translation
+from translations.utils import _get_standard_language
 
 
 __docformat__ = 'restructuredtext'
@@ -10,10 +12,13 @@ __docformat__ = 'restructuredtext'
 
 def generate_translation_form(translatable):
     """Return a translation form based on a translatable model."""
-    choices = translatable._get_translatable_fields_choices()
+    fields = translatable._get_translatable_fields_choices()
+    default = _get_standard_language(settings.LANGUAGE_CODE)
+    languages = [lang for lang in settings.LANGUAGES if lang[0] != default]
 
     class TranslationForm(forms.ModelForm):
-        field = forms.ChoiceField(choices=choices)
+        field = forms.ChoiceField(choices=fields)
+        language = forms.ChoiceField(choices=languages)
 
         class Meta:
             model = Translation
