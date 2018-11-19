f4c11bc6f5e602af439f3303356528140b20fd67
Behzad B. Mokhtari <35877268+perplexionist@users.noreply.github.com>
2018-11-19 16:40:15 +0330

    `synctranslations` tests for `execute` and `get_content_types` pt1

--- tests/test_management/test_commands/test_synctranslations.py
+++ tests/test_management/test_commands/test_synctranslations.py
@@ -1,8 +1,43 @@
+from io import StringIO
+
 from django.test import TestCase
+
+from translations.management.commands.synctranslations import Command
 
 
 class CommandTest(TestCase):
     """Tests for `Command`."""
 
-    def test_something(self):
-        raise Exception('working')
+    def test_execute(self):
+        out = StringIO()
+        command = Command(stdout=out)
+        command.run_from_argv(['manage.py', 'synctranslations'])
+
+        self.assertIs(
+            hasattr(command, 'stdin'),
+            True
+        )
+
+    def test_get_content_types_with_no_app_label(self):
+        command = Command()
+        content_types = command.get_content_types()
+
+        self.assertListEqual(
+            sorted(
+                list(content_types.values_list('app_label', 'model')),
+                key=lambda x: (x[0], x[1])
+            ),
+            [
+                ('admin', 'logentry'),
+                ('auth', 'group'),
+                ('auth', 'permission'),
+                ('auth', 'user'),
+                ('contenttypes', 'contenttype'),
+                ('sample', 'city'),
+                ('sample', 'continent'),
+                ('sample', 'country'),
+                ('sample', 'timezone'),
+                ('sessions', 'session'),
+                ('translations', 'translation'),
+            ]
+        )
