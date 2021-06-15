448ae55e40af08c5e1a1a1df28aec4b3a9282b36
Hasan Ramezani <hasan.r67@gmail.com>
2021-06-15 23:35:38 +0200

    Remove support for Django < 2.2

--- setup.py
+++ setup.py
@@ -21,8 +21,6 @@ setuptools.setup(
     classifiers=[
         'Development Status :: ' + info['release']['classifier'],
         'Framework :: Django',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
