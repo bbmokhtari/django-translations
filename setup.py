ed322eb24fb0e5dc6a62522ad023d7123023f4ac
Hasan Ramezani <hasan.r67@gmail.com>
2021-06-18 10:32:47 +0200

    Add Django 3.2 support.

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
         'Framework :: Django',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
