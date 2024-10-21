8a30902dc4e9ff84d83b376ec5d70e45118f41ae
Behzad B. Mokhtari <35877268+bbmokhtari@users.noreply.github.com>
2024-10-21 18:30:06 +0330

    remove caching of content types at the start

--- translations/apps.py
+++ translations/apps.py
@@ -8,12 +8,3 @@ except ImportError:
 class TranslationsConfig(AppConfig):
     name = 'translations'
     verbose_name = _('translations')
-
-    def ready(self):
-        try:
-            # cache all content types at the start
-            from django.contrib.contenttypes.models import ContentType
-            models = [ct.model_class() for ct in ContentType.objects.all()]
-            ContentType.objects.get_for_models(*models)
-        except Exception:
-            pass
